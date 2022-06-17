6e5da7d2a9e61c0a4c830805bd8a95d10bd4a5c3
CoolCreasu <svenboeckx2@gmail.com>
2022-06-17 09:16:37 +0200

    Nullable Errros still in the code need to fix.

--- Compiler/Token.cs
+++ Compiler/Token.cs
@@ -18,8 +18,16 @@ namespace Compiler
 
         public static TokenType? CheckIfKeyword(string tokenText)
         {
-            // If the string matches a Name in the TokenType enum and if the value >= 100 && value <= 200
-            return TokenType.IDENT;
+            // If the string matches a Name in the TokenType enum and if the value >= 100 && value < 200
+            if (Enum.TryParse(tokenText, true, out TokenType type))
+            {
+                var enumName = Enum.Parse<TokenType>(tokenText);
+                int value = (int)enumName;
+                if (value >= 100 && value < 200)
+                {
+                    return enumName;
+                }
+            }
             // Else
             return null;
         }
