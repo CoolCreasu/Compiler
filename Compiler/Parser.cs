bc2fe28c555a275e9b308ba287aa9cbd403dcd6e
CoolCreasu <svenboeckx2@gmail.com>
2022-06-17 20:24:49 +0200

    Edited parser code

--- Compiler/Parser.cs
+++ Compiler/Parser.cs
@@ -9,27 +9,40 @@ namespace Compiler
     // The parser keeps track of current token and checks if the code matches the grammar.
     internal class Parser
     {
+        Lexer _lexer;
+        Token _currentToken;
+        Token _peekToken;
+
         public Parser(Lexer lexer)
         {
-
+            _lexer = lexer;
         }
 
-        // Return true if the token matches.
-        public void CheckToken(TokenType tokenType)
+        // Return true if the current token matches.
+        public bool CheckToken(TokenType tokenKind)
         {
-
+            return (tokenKind == _currentToken.TokenKind);
         }
 
         // Return true if the next token matches.
-        public void CheckPeek(TokenType tokenType)
+        public bool CheckPeek(TokenType tokenKind)
         {
-
+            return (tokenKind == _peekToken.TokenKind);
         }
 
         // Try to match current Token. If not, error. Advances the current token.
-        public void Match(TokenType tokenType)
+        public void Match(TokenType tokenKind)
         {
+            if (!CheckToken(tokenKind))
+                Abort("Expected " + tokenKind.ToString() + ", got " + _currentToken.TokenKind.ToString());
+            NextToken();
+        }
 
+        public void NextToken()
+        {
+            _currentToken = _peekToken;
+            _peekToken = _lexer.GetToken();
+            // No need to worry about passing the EOF, lexer handles that.
         }
 
         public void Abort(string message)
