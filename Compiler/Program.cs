2123d5b91d9a367c3d028b8d01c70ff57d97dfca
CoolCreasu <svenboeckx2@gmail.com>
2022-06-17 20:27:47 +0200

    Edited main code

--- Compiler/Program.cs
+++ Compiler/Program.cs
@@ -4,16 +4,21 @@
     {
         private static string _code = "+-123 9.8654*/*/UP LET BE IS GOTO LABEL";
         private static Lexer _lexer = new Lexer(_code);
+        private static Parser _parser = new Parser(_lexer);
 
 
         static void Main(string[] args)
         {
+            /*
             Token? token = _lexer.GetToken();
             while (token.TokenKind != TokenType.EOF)
             {
                 Console.WriteLine(token.TokenKind);
                 token = _lexer.GetToken();
             }
+            */
+            //_parser.Program();
+            Console.WriteLine("Parsing complete");
         }
     }
 }
