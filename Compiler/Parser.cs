e383542952c95909b6050b4a0eb42c19919e8642
CoolCreasu <svenboeckx2@gmail.com>
2022-06-17 20:05:55 +0200

    Created parser class

---
+++ Compiler/Parser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler
+{
+    // The parser keeps track of current token and checks if the code matches the grammar.
+    internal class Parser
+    {
+        public Parser(Lexer lexer)
+        {
+
+        }
+
+        // Return true if the token matches.
+        public void CheckToken(TokenType tokenType)
+        {
+
+        }
+
+        // Return true if the next token matches.
+        public void CheckPeek(TokenType tokenType)
+        {
+
+        }
+
+        // Try to match current Token. If not, error. Advances the current token.
+        public void Match(TokenType tokenType)
+        {
+
+        }
+
+        public void Abort(string message)
+        {
+            throw new Exception(message);
+        }
+    }
+}
